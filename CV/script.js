70f1dae36eb833cee3c4e162848b1052a70d7aab
Cong-Khanh <nguyencongkhanh01306@gmail.com>
2023-07-29 20:04:00 +0700

    first commit

---
+++ CV/script.js
@@ -0,0 +1,28 @@
+document.getElementById('next').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').appendChild(lists[0]);
+}
+document.getElementById('prev').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').prepend(lists[lists.length - 1]);
+}
+function openCity(evt, cityName) {
+    
+    var i, tabcontent, tablinks;
+  
+    
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
